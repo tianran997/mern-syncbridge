09168f8ae9362844ea15ef41956e7f7d34dac98b
Tianran Bu <tianran997@gmail.com>
2025-08-19 10:00:39 -0700

    prepared for deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,6 @@
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
-const api = BASE_URL;
-export default api;
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001/api"
+    : "https://mern-syncbridge.onrender.com/api";
+
+export default BASE_URL;
