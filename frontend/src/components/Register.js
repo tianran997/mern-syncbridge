982f8a617f69c982afea9ac3caeafc1a5a4e9dd1
Tianran Bu <tianran997@gmail.com>
2025-08-19 09:51:22 -0700

    prepared for deployment

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import api from '..config.js';
+import api from '../config.js';
 
 
 const API_BASE_URL = api;
